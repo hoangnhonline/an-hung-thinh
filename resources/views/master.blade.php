4df751985a63e4fc28ae2258493f5ac06a34da0f
Sang Lai <benvjp619@gmail.com>
2016-09-15 14:06:03 +0700

    a

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -30,38 +30,38 @@
 <meta property="og:image" content="upload/hinhanh/11532792.png" />
 <meta property="og:description" content="dahoacuong.co" />
 <!-------------------------------------->
-<link href="upload/hinhanh/11532792.png" rel="shortcut icon" type="image/x-icon" />
-<link rel="stylesheet" type="text/css" href="assets/bootstrap-3.2.0/css/bootstrap.css"/>
-<link rel="stylesheet" type="text/css" href="assets/font.css"/>
-<link rel="stylesheet" type="text/css" href="assets/fancybox/jquery.fancybox.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="assets/menu/menumaker.css"/>
-<link rel="stylesheet" type="text/css" href="assets/mycss.css"/>
-<link rel="stylesheet" type="text/css" href="assets/Font-Awesome-master4.5/css/font-awesome.css"/>
-<link rel="stylesheet" type="text/css" href="assets/owl.carousel2.4/owl.carousel.css"/>
-<link rel="stylesheet" href="assets/simplyscroll/jquery.simplyscroll.css" media="all" type="text/css">
-<link rel="stylesheet" href="assets/animate/animate.css" media="all" type="text/css">
-<script type="text/javascript" src="js/jquery.min.1.10.js"></script>
-<script type="text/javascript" src="assets/bootstrap-3.2.0/js/bootstrap.js"></script>
-<script type="text/javascript" src="assets/bootstrap-3.2.0/js/jquery-ui.js"></script>
-<script type="text/javascript" src="assets/fancybox/jquery.fancybox.js"></script>
-<script type="text/javascript" src="assets/menu/menumaker.js"></script>
-<script type="text/javascript" src="assets/myscript.js"></script>
-<script type="text/javascript" src="assets/owl.carousel2.4/owl.carousel.js"></script>
-<script type="text/javascript" src="assets/simplyscroll/jquery.simplyscroll.js"></script>
-<script type="text/javascript" src="assets/animate/wow.min.js"></script>
-<script src="assets/animate/wow_animate.js"></script>
-<script type="text/javascript" src="assets/script_scroll/plugins-scroll.js"></script>
+<link href="{{ URL::asset('upload/hinhanh/11532792.png') }}" rel="shortcut icon" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/bootstrap-3.2.0/css/bootstrap.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/font.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/fancybox/jquery.fancybox.css') }}" media="screen" />
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/menu/menumaker.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/mycss.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/Font-Awesome-master4.5/css/font-awesome.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/owl.carousel2.4/owl.carousel.css') }}"/>
+<link rel="stylesheet" href="{{ URL::asset('assets/simplyscroll/jquery.simplyscroll.css') }}" media="all" type="text/css">
+<link rel="stylesheet" href="{{ URL::asset('assets/animate/animate.css') }}" media="all" type="text/css">
+<script type="text/javascript" src="{{ URL::asset('js/jquery.min.1.10.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/bootstrap-3.2.0/js/bootstrap.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/bootstrap-3.2.0/js/jquery-ui.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/fancybox/jquery.fancybox.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/menu/menumaker.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/myscript.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/owl.carousel2.4/owl.carousel.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/simplyscroll/jquery.simplyscroll.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/animate/wow.min.js') }}"></script>
+<script src="{{ URL::asset('assets/animate/wow_animate.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/script_scroll/plugins-scroll.js') }}"></script>
 
 <!--Tooltip Ajax-->
-<link href="assets/Tooltip_ajax/tootstip.css" rel="stylesheet" type="text/css" />
-<script src="assets/Tooltip_ajax/ajax.js" type="text/javascript"></script>
-<script src="assets/Tooltip_ajax/ajax-dynamic-content.js" type="text/javascript"></script>
-<script src="assets/Tooltip_ajax/home.js" type="text/javascript"></script>
+<link href="{{ URL::asset('assets/Tooltip_ajax/tootstip.css') }}" rel="stylesheet" type="text/css" />
+<script src="{{ URL::asset('assets/Tooltip_ajax/ajax.js') }}" type="text/javascript"></script>
+<script src="{{ URL::asset('assets/Tooltip_ajax/ajax-dynamic-content.js') }}" type="text/javascript"></script>
+<script src="{{ URL::asset('assets/Tooltip_ajax/home.js') }}" type="text/javascript"></script>
 <!--End Tooltip Ajax-->
 
-<link href="css/common.css" rel="stylesheet" type="text/css">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="(min-width: 980px)">
-<link href="css/style_mobile.css" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('css/common.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('css/style.css') }}" rel="stylesheet" type="text/css" media="(min-width: 980px)">
+<link href="{{ URL::asset('css/style_mobile.css') }}" rel="stylesheet" type="text/css">
 </head>
 
 
