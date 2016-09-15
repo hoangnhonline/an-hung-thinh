e564018231cccee66ebbf5e44e4c4d9ffbd02709
Sang Lai <benvjp619@gmail.com>
2016-09-15 12:57:41 +0700

    a

---
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Http\Controllers\Frontend;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Helper, File, Session, DB;
+
+class HomeController extends Controller
+{  
+    /**
+    * Display a listing of the resource.
+    *
+    * @return Response
+    */
+    public function index(Request $request)
+    {
+        
+        return view('pages.index');
+    }
+    public function lienhe(Request $request)
+    {
+        
+        return view('pages.lienhe');
+    }
+    public function sanpham(Request $request)
+    {
+        
+        return view('pages.sanpham');
+    }
+    public function loaisp($slug, $id)
+    {
+        $tenloaisp = DB::table('category')->where('slug',$slug)->get();
+        $tensp = DB::table('product')->where('cate_id',$id)->get();
+        return view('pages.loaisp', compact('tenloaisp', 'tensp'));
+    }
+    public function chitietsp($id, $slug)
+    {
+        $chitietsp = DB::table('product')->where('id',$id)->where('slug',$slug)->get();
+        
+        return view('pages.chitiet', compact('chitietsp'));
+    }
+    public function gioithieu(Request $request)
+    {
+        
+        return view('pages.gioithieu');
+    }
+
+}
