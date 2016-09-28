3bcfc767bb8ec546e7e5f9041a7c2fc1cdf9d935
HoangNH <hoangnh@evolableasia.vn>
2016-09-28 18:51:43 +0700

    update be

--- resources/views/layout/backend.blade.php
+++ resources/views/layout/backend.blade.php
@@ -24,15 +24,7 @@
   <link rel="stylesheet" href="{{ URL::asset('be/dist/css/sweetalert2.min.css') }}">  
   <link rel="stylesheet" href="{{ URL::asset('be/dist/css/select2.min.css') }}">
   <!-- Morris chart -->
-  
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="{{ URL::asset('be/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="{{ URL::asset('be/plugins/datepicker/datepicker3.css') }}">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{ URL::asset('be/plugins/daterangepicker/daterangepicker.css') }}">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="{{ URL::asset('be/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+4
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -83,28 +75,6 @@
 <script src="{{ URL::asset('be/dist/js/select2.min.js') }}"></script>
 <script src="{{ URL::asset('be/dist/js/es6-promise.min.js') }}"></script>
 <script src="{{ URL::asset('be/dist/js/finally.js') }}"></script>
-
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-
-<!-- Sparkline -->
-<script src="{{ URL::asset('be/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
-<!-- jvectormap -->
-<script src="{{ URL::asset('be/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-<script src="{{ URL::asset('be/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{ URL::asset('be/plugins/knob/jquery.knob.js') }}"></script>
-<!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="{{ URL::asset('be/plugins/daterangepicker/daterangepicker.js') }}"></script>
-<!-- datepicker -->
-<script src="{{ URL::asset('be/plugins/datepicker/bootstrap-datepicker.js') }}"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="{{ URL::asset('be/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
-<!-- Slimscroll -->
-<script src="{{ URL::asset('be/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
-<!-- FastClick -->
-<script src="{{ URL::asset('be/plugins/fastclick/fastclick.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ URL::asset('be/dist/js/app.min.js') }}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
