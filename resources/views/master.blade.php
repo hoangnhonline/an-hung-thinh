040378b9daf3fa58349c1878db83d4e62a962076
HoangNH <hoangnh@evolableasia.vn>
2016-10-03 17:13:43 +0700

    favicon

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,7 +3,8 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Đá Hoa Cương</title>
+<title>Trang chủ dagranit.vn - Một sản phẩm của An Hưng Thịnh</title>
+<link rel="shortcut icon" type="image/x-icon" href="{{ URL::asset('images/favicon.png') }}">
 <meta name="description" content="">
 <meta name="keywords" content="">
 <meta name="robots" content="index,follow" />
